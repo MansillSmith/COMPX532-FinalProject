9cfc2a3826370bfeb2ba1e3e5244a75b5bfc9d6d
mjs117 <mjs117@students.waikato.ac.nz>
2022-05-24 18:40:44 +1200

    Reads internal Json file

--- GolfReview/GolfReview/MainWindow.xaml.cs
+++ GolfReview/GolfReview/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,9 +23,28 @@ namespace GolfReview
     /// </summary>
     public partial class MainWindow : Window
     {
+        const string SCORES_DATA_PATH = "GolfReview.Data.Scores.json";
+
         public MainWindow()
         {
             InitializeComponent();
+
+            CreateScoreGrid();
         }
+
+        public void CreateScoreGrid()
+        {
+            //Read the Data JSON File
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            JObject scoresJSON = null;
+            using(StreamReader reader = new StreamReader(assembly.GetManifestResourceStream(SCORES_DATA_PATH)))
+            {
+                scoresJSON = JObject.Parse(reader.ReadToEnd());
+            }
+
+
+            Console.WriteLine(scoresJSON.ToString());
+        }
+
     }
 }
