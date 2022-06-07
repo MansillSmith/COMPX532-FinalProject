8f57275eb595a11f3b1221cfe4bf2ad230e5c0d9
mjs117 <mjs117@students.waikato.ac.nz>
2022-06-07 19:55:58 +1200

    Displays all 18 holes

--- GolfReview/GolfReview/Player.cs
+++ GolfReview/GolfReview/Player.cs
@@ -76,6 +76,84 @@ namespace GolfReview
             get { return _hole6; }
         }
 
+        private int _hole7;
+        public int Hole7
+        {
+            get { return _hole7; }
+        }
+
+        private int _hole8;
+        public int Hole8
+        {
+            get { return _hole8; }
+        }
+
+        private int _hole9;
+        public int Hole9
+        {
+            get { return _hole9; }
+        }
+
+        private int _hole10;
+        public int Hole10
+        {
+            get { return _hole10; }
+        }
+
+        private int _hole11;
+        public int Hole11
+        {
+            get { return _hole11; }
+        }
+
+        private int _hole12;
+        public int Hole12
+        {
+            get { return _hole12; }
+        }
+
+        private int _hole13;
+        public int Hole13
+        {
+            get { return _hole13; }
+        }
+
+        private int _hole14;
+        public int Hole14
+        {
+            get { return _hole14; }
+        }
+
+        private int _hole15;
+        public int Hole15
+        {
+            get { return _hole15; }
+        }
+
+        private int _hole16;
+        public int Hole16
+        {
+            get { return _hole16; }
+        }
+
+        private int _hole17;
+        public int Hole17
+        {
+            get { return _hole17; }
+        }
+
+        private int _hole18;
+        public int Hole18
+        {
+            get { return _hole18; }
+        }
+
+        private int _total = 0;
+        public int Total
+        {
+            get { return _total; }
+        }
+
         private BitmapImage _image;
 
         public BitmapImage Image
@@ -115,8 +193,12 @@ namespace GolfReview
 
             foreach (var i in jObject[nameof(Player.Holes)])
             {
-                Holes.Add(new Hole((JObject)i));
+                Hole h = new Hole((JObject)i);
+                this._total += h.Score;
+                Holes.Add(h);
             }
+
+
 
             this._hole1 = Holes[0].Score;
             this._hole2 = Holes[1].Score;
@@ -124,6 +206,18 @@ namespace GolfReview
             this._hole4 = Holes[3].Score;
             this._hole5 = Holes[4].Score;
             this._hole6 = Holes[5].Score;
+            this._hole7 = Holes[6].Score;
+            this._hole8 = Holes[7].Score;
+            this._hole9 = Holes[8].Score;
+            this._hole10 = Holes[9].Score;
+            this._hole11 = Holes[10].Score;
+            this._hole12 = Holes[11].Score;
+            this._hole13 = Holes[12].Score;
+            this._hole14 = Holes[13].Score;
+            this._hole15 = Holes[14].Score;
+            this._hole16 = Holes[15].Score;
+            this._hole17 = Holes[16].Score;
+            this._hole18 = Holes[17].Score;
             this.Selected = true;
 
             if (this.Name != null)
